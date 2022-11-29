dbf0d06aeb6df671b2a19426d008d4af48a9eed2
sankar1999 <sankarsathya1973@gmail.com>
2022-11-29 19:29:52 +0530

    Added Docker

--- Docker Repository/docker/src/main/java/com/docker/controller/DockerController.java
+++ Docker Repository/docker/src/main/java/com/docker/controller/DockerController.java
@@ -1,4 +1,13 @@
 package com.docker.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DockerController {
+
+    @GetMapping("/")
+    public String helloDocker() {
+        return "Hello Docker...";
+    }
 }
